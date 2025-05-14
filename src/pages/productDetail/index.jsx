b36765cde1d47d7d19144a51be5a25d36b068a78
dahhyeon <dahhyeon22@gmail.com>
2025-05-14 22:10:01 +0900

    chore: 불필요한 코드 제거

--- src/pages/productDetail/index.jsx
+++ src/pages/productDetail/index.jsx
@@ -1,28 +1,5 @@
-import Button from '@/components/common/Button/Button';
-import styled from 'styled-components';
-
-const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 20px;
-  padding: 10px 20px;
-`;
-
 const ProductDetail = () => {
-  return (
-    <Wrapper>
-      <Button>로그인하기</Button>
-      <Button variant="primary" size="lg" fullWidth>
-        로그인하기
-      </Button>
-      <Button variant="secondary" size="md">
-        로그인하기
-      </Button>
-      <Button variant="primary" size="sm" rounded="full">
-        글쓰기
-      </Button>
-    </Wrapper>
-  );
+  return <div>상품 상세페이지</div>;
 };
 
 export default ProductDetail;
