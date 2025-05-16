002d05e422ba6363a934f8b21963409ad61ea2f7
LeeSoohyun026 <soohyun5060@gmail.com>
2025-05-16 21:35:05 +0900

    Revert "#2 Feat: 상품 등록/수정하기 UI 구현"

--- src/constants/category.js
+++
@@ -1,7 +0,0 @@
-const category = [
-  { label: '의류', value: '의류' },
-  { label: '책/서적', value: '책/서적' },
-  { label: 'IT/전자기기', value: 'IT/전자기기' },
-];
-
-export default category;
--- src/routes/route.jsx
+++ src/routes/route.jsx
@@ -1,8 +1,6 @@
 import Home from '@/pages/home';
 import ProductDetail from '@/pages/productDetail';
 import Signup from '@/pages/signup';
-import ProductCreate from '@/pages/productCreate';
-import ProductEdit from '@/pages/productEdit';
 import { createBrowserRouter } from 'react-router-dom';
 
 const router = createBrowserRouter([
@@ -13,14 +11,6 @@ const router = createBrowserRouter([
   {
     path: '/home',
     element: <Home />,
-  },
-  {
-    path: '/product/new',
-    element: <ProductCreate />,
-  },
-  {
-    path: '/product/:id/edit',
-    element: <ProductEdit />,
   },
   {
     path: '/product/detail',
