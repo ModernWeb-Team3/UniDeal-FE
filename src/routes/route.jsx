311da92850e005faa770a2581b1795541fe59b19
joyujin03 <whdbwls3399@gmail.com>
2025-05-18 22:44:23 +0900

    feat: 진입화면 ui 및 route 설정

--- src/routes/route.jsx
+++ src/routes/route.jsx
@@ -1,16 +1,14 @@
-import Home from '@/pages/home';
-import Signup from '@/pages/signup';
+
 import { createBrowserRouter } from 'react-router-dom';
+
+
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <Signup />,
+    path: '/',               
+    element: <EntryPage/>,   
   },
-  {
-    path: '/home',
-    element: <Home />,
-  },
+  
 ]);
 
 export default router;
