506aceae786a28b8fa8123a5d7c3ff4def77209d
Dahyeon <dahhyeon22@gmail.com>
2025-05-29 14:19:40 +0900

    feat: 리프레쉬 요청 인터셉터 추가

--- src/api/api.js
+++ src/api/api.js
@@ -2,13 +2,13 @@ import axios from 'axios';
 
 const BASE_URL = import.meta.env.VITE_API_URL;
 
-// 인스턴스
 const api = axios.create({
   baseURL: BASE_URL,
   timeout: 5000,
   headers: {
     'Content-Type': 'application/json',
   },
+  withCredentials: true,
 });
 
 // 요청 인터셉터 설정
@@ -21,7 +21,43 @@ api.interceptors.request.use(
     return config;
   },
   (error) => {
-    console.error('요청 에러:', error);
+    console.error('요청 인터셉터 에러:', error);
+    return Promise.reject(error);
+  },
+);
+
+// 응답 인터셉터: accessToken 만료 시 자동 재발급
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      try {
+        // 리프레시 요청
+        const res = await axios.post(
+          `${BASE_URL}/reissue`,
+          {},
+          {
+            withCredentials: true,
+          },
+        );
+
+        const newAccessToken = res.data?.data?.jwtToken;
+        if (newAccessToken) {
+          localStorage.setItem('accessToken', newAccessToken);
+          originalRequest.headers['Authorization'] = `Bearer ${newAccessToken}`;
+          return api(originalRequest); //  원래 요청 재시도
+        }
+      } catch (refreshError) {
+        console.error('리프레시 실패:', refreshError);
+        localStorage.removeItem('accessToken');
+        window.location.href = '/';
+        return Promise.reject(refreshError);
+      }
+    }
+
     return Promise.reject(error);
   },
 );
